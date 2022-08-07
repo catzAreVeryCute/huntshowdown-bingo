75aa77bd5ad23ffb6ff4bf7a9a651a4e7bde271f
catzAreVeryCute <spyroisbeast@gmail.com>
2022-08-06 20:17:31 -0500

    Changed color of markers upon a bingo

--- main.py
+++ main.py
@@ -15,6 +15,8 @@ HEIGHT = 1070
 WINDOW_SIZE = "1004x1070"
 TITLE = "Hunt: Showdown Bingo"
 BG_COLOR = "black"
+MARKER_COLOR = "yellow" 
+BINGO_COLOR = "red"
 
 class Main(tk.Tk):
     def __init__(self, *args, **kwargs):
@@ -36,12 +38,17 @@ class Main(tk.Tk):
         self.card_width = 1000
         self.card_height = 994
 
+        # Calculate width and height of tile and thickness of purple borders
+        self.border_thickness = round((self.card_width * 0.10385) / 6)
+        self.tile_width = round((self.card_width - (self.card_width * 0.10385)) / 5)
+        self.tile_height = round((self.card_height - (self.card_height * 0.1045)) / 5)
+
         # Restrict the size of the GUI so we don't have resize issues
         self.minsize(WIDTH, HEIGHT)
         self.maxsize(WIDTH, HEIGHT)
 
-        # When mouse 1 (left click) is clicked, run the get_mouse_pos function
-        self.bind("<Button 1>", self.get_mouse_pos)
+        # When mouse 1 (left click) is clicked, run the clicked_on_card function
+        self.bind("<Button 1>", self.clicked_on_card)
 
         # Sets window size and title
         self.geometry(WINDOW_SIZE) 
@@ -69,10 +76,7 @@ class Main(tk.Tk):
         add_tiles_btn['font'] = BUTTON_FONT
 
         # The current bingo card as an Image object
-        self.card = Image.open("./assets/card.png") 
-
-        # A copy of the current bingo card. Needed for resizing 
-        # self.card_copy = self.card.copy()         
+        self.card = Image.open("./assets/card.png")        
 
         # Resize bingo card according to the current window size
         self.card = self.card.resize((self.card_width, self.card_height))  
@@ -113,7 +117,12 @@ class Main(tk.Tk):
         self.marked = [[False for x in range(5)] for y in range(5)] 
 
 
-    def get_mouse_pos(self, event):
+    def clicked_on_card(self, event):
+        self.set_mouse_pos(event)
+        self.mark_card()
+
+
+    def set_mouse_pos(self, event):
         """
         If the bingo card is clicked on, we determine where the card was clicked on in x and y pixel coordinates. 
         """
@@ -126,57 +135,155 @@ class Main(tk.Tk):
             self.mouse_x = event.x
             self.mouse_y = event.y
 
-            self.mark_card()
+        
+    def draw(self, col, row, color):
 
+        
+        # x1 is the x coordinate for the left side of the tile box
+        # x2 is the x coordinate for the right side of the tile box
+        # y1 is the y coordinate for the top of the tile box
+        # y2 is the y coordinate for the bottom of the tile box
+        # (1*col) + 1 and (1*row) + 1 are just there to account for error
+        x1 = (col * self.tile_width) + (col * self.border_thickness) + self.border_thickness 
+        y1 = (row * self.tile_height) + (row * self.border_thickness) + self.border_thickness 
+        x2 = (col * self.tile_width) + (col * self.border_thickness) + self.border_thickness + self.tile_width + (1 * col) + 1
+        y2 = (row * self.tile_height) + (row * self.border_thickness) + self.border_thickness + self.tile_height + (1 * row) + 1
+
+        if (self.marked[col][row]):
+
+            # draw width is the width of the bingo marker
+            draw_width = round(self.border_thickness / 2)
+            offset = round(draw_width / 2)
+
+            # draws the square
+            self.canvas.create_line(x1, y1+offset, x2, y1+offset, fill=color, width=draw_width, tags=(f"col{col}row{row}", "marker"))
+            self.canvas.create_line(x2-offset, y1, x2-offset, y2, fill=color, width=draw_width, tags=(f"col{col}row{row}", "marker"))
+            self.canvas.create_line(x2, y2-offset, x1, y2-offset, fill=color, width=draw_width, tags=(f"col{col}row{row}", "marker"))
+            self.canvas.create_line(x1+offset, y2, x1+offset, y1, fill=color, width=draw_width, tags=(f"col{col}row{row}", "marker"))
+
+            # draws the "x"
+            self.canvas.create_line(x1+offset, y1+offset, x2-offset, y2-offset, fill=color, width=draw_width, tags=(f"col{col}row{row}", "marker"))
+            self.canvas.create_line(x2-offset, y1+offset, x1+offset, y2-offset, fill=color, width=draw_width, tags=(f"col{col}row{row}", "marker"))
+
+            # square black border
+            border_draw_width = round(self.border_thickness / 4)
+            border_offset = round(border_draw_width / 2) 
+            # self.canvas.create_line(x1, y1+border_offset, x2, y1+border_offset, fill="black", width=border_draw_width, tags=(f"col{col}row{row}", "marker"))
+            # self.canvas.create_line(x2-border_offset, y1, x2-border_offset, y2, fill="black", width=border_draw_width, tags=(f"col{col}row{row}", "marker"))
+            # self.canvas.create_line(x2, y2-border_offset, x1, y2-border_offset, fill="black", width=border_draw_width, tags=(f"col{col}row{row}", "marker"))
+            # self.canvas.create_line(x1+border_offset, y2, x1+border_offset, y1, fill="black", width=border_draw_width, tags=(f"col{col}row{row}", "marker"))
+
+        else:
+            self.canvas.delete(f"col{col}row{row}")
+
+    
+
+    def check_for_bingo(self, col, row):
+        row_ = [row_[row] for row_ in self.marked]
+        col_ = self.marked[col]
+
+        # Check for a vertical bingo
+        if (all(col_)):
+            for r in range(0, 5):
+                self.canvas.delete(f"col{col}row{r}")
+                self.draw(col, r, BINGO_COLOR)
+        
+        # Check for a horizontal bingo
+        if (all(row_)):
+            for c in range(0, 5):
+                self.canvas.delete(f"col{c}row{row}")
+                self.draw(c, row, BINGO_COLOR)
+
+        # Check for diagonal bingo; top left to bottom right
+        if (col == row):
+
+            # Check if there's a bingo
+            bingo = True
+            for i in range(0, 5):
+                bingo = bingo and self.marked[i][i]
+
+            # Change color of markers if there's a diagonal bingo
+            if (bingo):
+                for i in range(0, 5):
+                    self.canvas.delete(f"col{i}row{i}")
+                    self.draw(i, i, BINGO_COLOR)
+
+        # Check for diagonal bingo; bottom left to top right
+        if (row + col == 4):
+
+            # Check if there's a diagonal bingo
+            bingo = True
+            for i in range(0, 5):
+                bingo = bingo and self.marked[i][4 - i]
+
+            # Change color of markers if there's a diagonal bingo
+            if (bingo):
+                for i in range(0, 5):
+                    self.canvas.delete(f"col{i}row{4 - i}")
+                    self.draw(i, 4 - i, BINGO_COLOR)
+
+
+    def undo_bingo(self, col, row):
+        row_ = [row_[row] for row_ in self.marked]
+        col_ = self.marked[col]
+
+        """Two issues. First, need to check that the column/row has at least 
+        4 other marked tiles before we change them back to yellow. if not leave em
+        
+        Second, we need to make sure we aren't undoing a colored bingo from another
+        row / col. So we need to check that as well"""
+
+        # Undoing a column bingo
+        for r in range(0, 5):
+            row_ = [row_[r] for row_ in self.marked]
+            if (self.marked[col][r] and not all(row_)):
+                self.canvas.delete(f"col{col}row{r}")
+                self.draw(col, r, MARKER_COLOR)
+        
+        # Undoing a row bingo
+        for c in range(0, 5):
+            col_ = self.marked[c]
+            if (self.marked[c][row] and not all(col_)):
+                self.canvas.delete(f"col{c}row{row}")
+                self.draw(c, row, MARKER_COLOR)
+        
 
     def mark_card(self):
-        border_thickness = round((self.card_width * 0.10385) / 6)
-        tile_width = round((self.card_width - (self.card_width * 0.10385)) / 5)
-        tile_height = round((self.card_height - (self.card_height * 0.1045)) / 5)
-
-        col = 0
         x = self.mouse_x
         y = self.mouse_y
 
+        col = 0
         while True:
-            if (x < border_thickness):
-                print("Clicked on the border!")
+            if (x < self.border_thickness): # This means we clicked on a border
                 return
-            elif (x < border_thickness + tile_width):
+            elif (x < self.border_thickness + self.tile_width): # Found the tile we clicked on
                 break
             else:
-                x = x - (tile_width + border_thickness)
+                x = x - (self.tile_width + self.border_thickness) # The tile is further down
                 col += 1
 
         row = 0
         while True:
-            if (y < border_thickness):
-                print("Clicked on the border!")
+            if (y < self.border_thickness): # Clicked on a border
                 return
-            elif (y < border_thickness + tile_width):
+            elif (y < self.border_thickness + self.tile_width): # Found the tile we clicked on
                 break
             else:
-                y = y - (tile_width + border_thickness)
+                y = y - (self.tile_width + self.border_thickness)
                 row += 1
         
         self.marked[col][row] = not self.marked[col][row]
 
-        draw_width = round(border_thickness / 2)
-        x1 = (col * tile_width) + (col * border_thickness) + border_thickness 
-        y1 = (row * tile_height) + (row * border_thickness) + border_thickness
-
-        x2 = (col * tile_width) + (col * border_thickness) + border_thickness + tile_width + (1 * col)
-        y2 = (row * tile_height) + (row * border_thickness) + border_thickness + tile_height + (1 * row)
-
+        # If we're marking a tile, we need to draw on the card and check for a bingo
+        # If we're unmarking a tile, we need to remove what we drew and check to see if we undid a bingo
         if (self.marked[col][row]):
-            self.canvas.create_polygon([x1, y1, x2, y1, (x2 - draw_width), (y1 + draw_width), (x1 + draw_width), (y1 + draw_width)], fill="blue", tags=(f"col{col}row{row}", "marker"))
-            self.canvas.create_polygon([x2, y1, x2, y2, (x2 - draw_width), (y2 - draw_width), (x2 - draw_width), (y1 + draw_width)], fill ="blue", tags=(f"col{col}row{row}", "marker"))
-            self.canvas.create_polygon([x2, y2, x1, y2, (x1 + draw_width), (y2 - draw_width), (x2 - draw_width), (y2 - draw_width)], fill="blue", tags=(f"col{col}row{row}", "marker"))
-            self.canvas.create_polygon([x1, y2, x1, y1, (x1 + draw_width), (y1 + draw_width), (x1 + draw_width), (y2 - draw_width)], fill="blue", tags=(f"col{col}row{row}", "marker"))
+            self.draw(col, row, MARKER_COLOR)
+            self.check_for_bingo(col, row)
         else:
-            self.canvas.delete(f"col{col}row{row}")
+            self.draw(col, row, MARKER_COLOR)
+            self.undo_bingo(col, row)
 
-
+        
     def get_new_bingo_card(self):
 
         # The relative path to where the tile images are stored
